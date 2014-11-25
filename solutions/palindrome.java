a3e6abf0cb80000fcd8d13ff1a1a95d97c4bf89c
Spencer Tipping <spencer@spencertipping.com>
2014-11-25 03:47:03 +0000

    Fixed bugs in my solutions (that string .length thing again, bad stuff!) and wrote a quick aside about not wearing suits to interviews

--- solutions/palindrome.java
+++ solutions/palindrome.java
@@ -1,5 +1,5 @@
 boolean palindrome(final String s) {
-  for (int i = 0, j = s.length - 1; i < j; ++i, --j)
+  for (int i = 0, j = s.length() - 1; i < j; ++i, --j)
     if (s.charAt(i) != s.charAt(j))
       return false;
   return true;
--- solutions/wordcount.java
+++ solutions/wordcount.java
@@ -1,7 +1,7 @@
 int wordcount(final String s) {
   boolean inWord = false;
   int     words  = 0;
-  for (int i = 0; i < s.length; ++i) {
+  for (int i = 0; i < s.length(); ++i) {
     final char c = s.charAt(i);
     if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z') {
       if (!inWord)
